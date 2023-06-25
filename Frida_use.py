cae1013014ba397f9a6dfc2c9eeefa82a6f38fc1
rootphantomer <rootphantomy@hotmail.com>
2023-06-25 16:16:41 +0800

    update frida_use.py

--- Frida_use.py
+++ Frida_use.py
@@ -6,9 +6,15 @@ def on_message(message, data):
     print(message)
 
 
+# attach 模式传入的是 APP 名称，spawn 模式传入的是 APP 包名
+# 方式一：attach 模式，已经启动的 APP
 session = frida.get_remote_device().attach("农发企业银行")
 # session = frida.get_usb_device().attach("com.android.chrome")
 # print(session)
+# 方式二，spawn 模式，重启 APP
+# device = frida.get_usb_device(-1)
+# pid = frida.get_remote_device().spawn(["owasp.mstg.uncrackable1"])
+# session = device.attach(pid)
 
 # with open("./js/cookieDump.js", "r", encoding="utf-8") as f:
 with open('./demo.js', "r", encoding="utf-8") as f:
