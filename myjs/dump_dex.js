f0449bfce7118d9176cc6f38f3a6ba6d1b860649
rootphantomer <rootphantomy@hotmail.com>
2023-07-04 16:45:55 +0800

    update dump_dex

--- myjs/dump_dex.js
+++ myjs/dump_dex.js
@@ -1,50 +1,84 @@
+function find_hook_fun() {
+  var fun_Name = "";
+  var libart = Module.findBaseAddress("libart.so"); //查找基地址
+  var exports = Module.enumerateExportsSync("libart.so");
+  for (var i = 0; i < exports.length; i++) {
+    if (exports[i].name.indexOf("OpenMemory") !== -1) {
+      fun_Name = exports[i].name;
+      console.log(
+        "导出模块名: " +
+          exports[i].name +
+          "\t\t偏移地址: " +
+          (exports[i].address - libart - 1)
+      );
+      break;
+    } else if (exports[i].name.indexOf("OpenCommon") !== -1) {
+      fun_Name = exports[i].name;
+      console.log(
+        "导出模块名: " +
+          exports[i].name +
+          "\t\t偏移地址: " +
+          (exports[i].address - libart - 1)
+      );
+      break;
+    }
+  }
+  return fun_Name;
+}
+
+function DexFileVerifier(Verify) {
+  var magic_03x = true;
+  var magic_Hex = [0x64, 0x65, 0x78, 0x0a, 0x30, 0x33, 0x35, 0x00];
+  for (var i = 0; i < 8; i++) {
+    if (Memory.readU8(ptr(Verify).add(i)) !== magic_Hex[i]) {
+      if (Memory.readU8(ptr(Verify).add(i)) === 0x37 || 0x38) {
+        console.log("new dex");
+      } else {
+        magic_03x = false;
+        break;
+      }
+    }
+  }
+  return magic_03x;
+}
+
 function dump_Dex(fun_Name, apk_Name) {
   if (fun_Name !== "") {
-    // 判断传入的参数 fun_Name 是否为空字符串
-    var hook_fun = Module.findExportByName("libart.so", fun_Name); // 在 libart.so 动态库中查找指定的导出函数地址
+    var hook_fun = Module.findExportByName("libart.so", fun_Name);
     Interceptor.attach(hook_fun, {
-      // 使用 Interceptor 对导出函数进行挂钩
       onEnter: function (args) {
-        // 进入函数之前的回调函数
-        var begin = 0; // 初始化一个变量用于保存 Dex 文件的起始地址
-        var dex_flag = false; // 初始化一个标志位，表示是否符合 Dex 文件的魔数
-        dex_flag = DexFileVerifier(args[0]); // 调用 DexFileVerifier 函数对传入的第一个参数进行验证
+        var begin = 0;
+        var dex_flag = false;
+        dex_flag = DexFileVerifier(args[0]);
         if (dex_flag === true) {
-          // 如果验证通过
-          begin = args[0]; // 保存 Dex 文件的起始地址
+          begin = args[0];
         }
         if (begin === 0) {
-          // 如果起始地址为 0
-          dex_flag = DexFileVerifier(args[1]); // 再次调用 DexFileVerifier 函数对传入的第二个参数进行验证
+          dex_flag = DexFileVerifier(args[1]);
           if (dex_flag === true) {
-            // 如果验证通过
-            begin = args[1]; // 保存 Dex 文件的起始地址
+            begin = args[1];
           }
         }
         if (dex_flag === true) {
-          // 如果验证通过
-          console.log("magic : " + Memory.readUtf8String(begin)); // 打印输出 Dex 文件的魔数
-          var address = parseInt(begin, 16) + 0x20; // 计算 Dex 文件大小字段的地址
-          var dex_size = Memory.readInt(ptr(address)); // 读取 Dex 文件大小
-          console.log("dex_size :" + dex_size); // 打印输出 Dex 文件的大小
-          var dex_path = "/data/data/" + apk_Name + "/" + dex_size + ".dex"; // 构建 Dex 文件保存路径
-          var dex_file = new File(dex_path, "wb"); // 创建一个文件对象用于写入 Dex 文件
-          dex_file.write(Memory.readByteArray(begin, dex_size)); // 将 Dex 文件内容写入文件
-          dex_file.flush(); // 刷新文件缓冲区
-          dex_file.close(); // 关闭文件
+          console.log("magic : " + Memory.readUtf8String(begin));
+          var address = parseInt(begin, 16) + 0x20;
+          var dex_size = Memory.readInt(ptr(address));
+          console.log("dex_size :" + dex_size);
+          var dex_path = "/data/data/" + apk_Name + "/" + dex_size + ".dex";
+          var dex_file = new File(dex_path, "wb");
+          dex_file.write(Memory.readByteArray(begin, dex_size));
+          dex_file.flush();
+          dex_file.close();
         }
       },
-      onLeave: function (retval) {
-        // 函数返回之后的回调函数
-      },
+      onLeave: function (retval) {},
     });
   } else {
-    console.log("Error: no hook function."); // 如果传入的参数 fun_Name 为空字符串，则打印错误消息
+    console.log("Error: no hook function.");
   }
 }
 
-// var fun_Name = find_hook_fun();
-// var apk_Name = 'com.xxx.xxx' // 给定要hook的apk
-
-// dump_Dex(fun_Name, apk_Name);
-// frida -U -f com.xxx.xxx -l dumpdex.js --no-pause
+var fun_Name = find_hook_fun();
+var apk_Name = "com.dodonew.online";
+dump_Dex(fun_Name, apk_Name);
+// frida -U -f com.dodonew.online -l dumpdex.js --no-pause
